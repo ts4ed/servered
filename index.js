ab0513d23a2e8585b9ae5a1a16f1a017861ca115
ts4ed <103256726+ts4ed@users.noreply.github.com>
2022-07-19 22:52:09 +0300

    setting up to push to Heroku

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import Post from './Post.js';
-import router from './router.js';
-import fileUpload from 'express-fileupload';
-import cors from 'cors';
+import express from "express";
+import mongoose from "mongoose";
+import Post from "./Post.js";
+import router from "./router.js";
+import fileUpload from "express-fileupload";
+import cors from "cors";
 
-const PORT = 5000;
-const DB_URL = `mongodb+srv://user:user@cluster0.8pnqpgo.mongodb.net/?retryWrites=true&w=majority`;
+const port = process.env.PORT || 3000;
 
 const app = express();
-// const cors = require('cors');
 
 app.use(express.json());
-app.use(express.static('static'));
+app.use(express.static("static"));
 app.use(cors());
 app.use(fileUpload({}));
 
-app.use('/api', router);
-// app.use('/users', usersRouter);
-// app.use('/api', router);
-// app.use('/api', router);
+app.use("/api", router);
 
 async function startApp() {
   try {
-    await mongoose.connect(DB_URL, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    });
-    app.listen(PORT, () => console.log('SERVER STARTED ON PORT' + PORT));
+    await mongoose.connect(
+      process.env.MONGODB_URI ||
+        "mongodb+srv://user:user@cluster0.8pnqpgo.mongodb.net/?retryWrites=true&w=majority"
+    );
+    app.listen(port, () => console.log("SERVER STARTED ON PORT" + port));
   } catch (error) {
     console.log(error);
   }
